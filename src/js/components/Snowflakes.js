8fd4460398d3994b15e6ad1de4b15ef4bd153eb3
David Eliauri <eliauri.david@gmail.com>
2024-10-25 14:17:59 +0300

    intermidiate changes

--- src/js/components/Snowflakes.js
+++ src/js/components/Snowflakes.js
@@ -1,51 +1,77 @@
 export default function Snowflakes() {
-    class Snowflake {
-        /** @type {number} */
-        #radius;
-        /** @type {number} */
-        #opacity;
-        /** @type {number} */
-        #posX;
-        /** @type {number} */
-        #posY;
-        /** @type {CanvasRenderingContext2D} */
-        #context;
-
-        /**
-         * @param {CanvasRenderingContext2D} context
-         */
-        constructor(context) {
-            // this.#posX =
-            //     Math.floor(Math.random() * (window.innerWidth - 0)) + 0;
-            //     this.#posY = Math.floor(Math.random() * (window.innerWidth - 0)) + 0;
-
-            this.#context = context;
-            this.#radius = 50;
-            this.#posX = 100;
-            this.#posY = 100;
-            this.#opacity = 0.5;
-
-            this.#draw();
-        }
-
-        #draw() {
-            this.#context.beginPath();
-
-            this.#context.arc(
-                this.#posX,
-                this.#posY,
-                this.#radius,
-                0,
-                Math.PI * 2
-            );
-            this.#context.fillStyle = "#000";
-            this.#context.fill();
-
-            this.#context.closePath();
-        }
-    }
-
     (() => {
+        function makeid(length = 5) {
+            let result = "";
+            const characters =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            const charactersLength = characters.length;
+            let counter = 0;
+            while (counter < length) {
+                result += characters.charAt(
+                    Math.floor(Math.random() * charactersLength)
+                );
+                counter += 1;
+            }
+            return result;
+        }
+
+        class Flake {
+            /** @type {string} */
+            id;
+            /** @type {number} */
+            positionX;
+            /** @type {number} */
+            positionY;
+            /** @type {number} */
+            speedX;
+            /** @type {number} */
+            speedY;
+
+            /**
+             * @param {number} positionX
+             * @param {number} positionY
+             */
+            constructor(positionX, positionY) {
+                this.positionX = positionX;
+                this.positionY = positionY;
+
+                this.speedY = 10;
+
+                this.id = makeid();
+
+                this.draw();
+            }
+
+            draw() {
+                context.beginPath();
+                context.arc(
+                    this.positionX + CELL_WIDTH / 2,
+                    this.positionY + CELL_WIDTH / 2,
+                    CELL_WIDTH / 2,
+                    0,
+                    Math.PI * 2
+                );
+                context.fillStyle = "#000";
+                context.fill();
+                context.closePath();
+            }
+
+            update() {
+                if (
+                    this.positionY + this.speedY + CELL_WIDTH / 2 >
+                    canvas.height
+                ) {
+                    this.draw();
+
+                    return;
+                }
+
+                this.positionY += this.speedY;
+
+                this.draw();
+            }
+        }
+
         const canvas = document.createElement("canvas");
         canvas.style.position = "absolute";
         canvas.style.top = "0px";
@@ -57,24 +83,200 @@ export default function Snowflakes() {
 
         const context = canvas.getContext("2d");
 
-        let y = 100;
+        function getRandomArbitrary(min, max) {
+            return Math.floor(Math.random() * (max - min) + min);
+        }
 
-        const animation = () => {
+        /**
+         * @typedef Grid
+         * @type {Grid}
+         */
+
+        const CELL_WIDTH = 10;
+
+        /** @type {Grid} */
+        let currentGrid,
+            /** @type {number} */
+            cols,
+            /** @type {number} */
+            rows;
+
+        /**
+         * @param {number} cols
+         * @param {number} rows
+         */
+        function makeGrid(cols, rows) {
+            /** @type {Grid} */
+            let array = new Array(cols);
+
+            for (let i = 0; i < array.length; i++) {
+                array[i] = new Array(rows);
+
+                for (let j = 0; j < array[i].length; j++) {
+                    array[i][j] = 0;
+                }
+            }
+
+            return array;
+        }
+
+        /**
+         * For test only
+         */
+        function drawCells() {
+            for (let i = 0; i < currentGrid.length; i++) {
+                for (let j = 0; j < currentGrid[i].length; j++) {
+                    context.beginPath();
+
+                    context.strokeStyle = "rgba(0, 0, 0, 0.3)";
+                    context.strokeRect(
+                        CELL_WIDTH * i,
+                        CELL_WIDTH * j,
+                        CELL_WIDTH,
+                        CELL_WIDTH
+                    );
+
+                    context.closePath();
+                }
+            }
+        }
+
+        function drawFlakes() {
+            for (let i = 0; i < currentGrid.length; i++) {
+                for (let j = 0; j < currentGrid[i].length; j++) {
+                    const cell = currentGrid[i][j];
+
+                    if (cell !== 1) {
+                        continue;
+                    }
+
+                    const leftTopCell = currentGrid[i - 1]?.[j - 1];
+                    const topCell = currentGrid[i][j - 1];
+                    const rightTopCell = currentGrid[i + 1]?.[j - 1];
+                    const rightCell = currentGrid[i + 1]?.[j];
+                    const rightBottomCell = currentGrid[i + 1]?.[j + 1];
+                    const bottomCell = currentGrid[i][j + 1];
+                    const leftBottomCell = currentGrid[i - 1]?.[j + 1];
+                    const leftCell = currentGrid[i - 1]?.[j];
+
+                    const hasFlakesFromEverySide =
+                        (leftTopCell === 1 || leftTopCell === undefined) &&
+                        (topCell === 1 || topCell === undefined) &&
+                        (rightTopCell === 1 || rightTopCell === undefined) &&
+                        (rightCell === 1 || rightCell === undefined) &&
+                        (rightBottomCell === 1 ||
+                            rightBottomCell === undefined) &&
+                        (bottomCell === 1 || bottomCell === undefined) &&
+                        (leftBottomCell === 1 ||
+                            leftBottomCell === undefined) &&
+                        (leftCell === 1 || leftCell === undefined);
+
+                    if (hasFlakesFromEverySide) {
+                        context.fillStyle = "#000";
+                        context.fillRect(
+                            CELL_WIDTH * i,
+                            CELL_WIDTH * j,
+                            CELL_WIDTH,
+                            CELL_WIDTH
+                        );
+
+                        continue;
+                    }
+
+                    context.beginPath();
+                    context.arc(
+                        CELL_WIDTH * i + CELL_WIDTH / 2,
+                        CELL_WIDTH * j + CELL_WIDTH / 2,
+                        CELL_WIDTH / 2,
+                        0,
+                        Math.PI * 2
+                    );
+                    context.fillStyle = "#000";
+                    context.fill();
+                    context.closePath();
+                }
+            }
+        }
+
+        /** @type {Flake[]} */
+        const flakes = [];
+
+        function setup() {
+            flakes.push(new Flake(0, 0));
+
+            cols = Math.ceil(canvas.width / CELL_WIDTH);
+            rows = Math.ceil(canvas.height / CELL_WIDTH);
+
+            currentGrid = makeGrid(cols, rows);
+
+            drawCells();
+
+            // const randomX = getRandomArbitrary(0, cols);
+
+            // currentGrid[randomX][0] = 1;
+
+            const randomX = getRandomArbitrary(1, cols - 1);
+
+            currentGrid[randomX - 1][0] = 1;
+            currentGrid[randomX][0] = 1;
+            currentGrid[randomX + 1][0] = 1;
+            currentGrid[randomX][1] = 1;
+
+            drawFlakes();
+
+            requestAnimationFrame(draw);
+        }
+
+        let lastFrameInMs = 0;
+
+        function draw(currentFrameInMs) {
+            // each 2 seconds call the createNewObject() function
+            if (!lastFrameInMs || currentFrameInMs - lastFrameInMs >= 1 * 500) {
+                lastFrameInMs = currentFrameInMs;
+
+                const randomX = getRandomArbitrary(1, cols - 1);
+
+                currentGrid[randomX - 1][0] = 1;
+                currentGrid[randomX][0] = 1;
+                currentGrid[randomX + 1][0] = 1;
+                currentGrid[randomX][1] = 1;
+            }
+
             context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-            y++;
+            flakes.forEach((flake) => {
+                flake.update();
+            });
 
-            context.beginPath();
+            // const newGrid = makeGrid(cols, rows);
 
-            context.arc(100, y, 50, 0, Math.PI * 2);
-            context.fillStyle = "#000";
-            context.fill();
+            // for (let i = 0; i < currentGrid.length; i++) {
+            //     for (let j = 0; j < currentGrid[i].length; j++) {
+            //         const cell = currentGrid[i][j];
+            //         const bellowCell = currentGrid[i]?.[j + 1];
 
-            context.closePath();
+            //         if (cell === 0) {
+            //             continue;
+            //         }
 
-            requestAnimationFrame(animation);
-        };
+            //         if (bellowCell === 0) {
+            //             newGrid[i][j + 1] = 1;
 
-        requestAnimationFrame(animation);
+            //             continue;
+            //         }
+
+            //         newGrid[i][j] = 1;
+            //     }
+            // }
+
+            // currentGrid = newGrid;
+
+            // drawCells();
+            // drawFlakes();
+
+            requestAnimationFrame(draw);
+        }
+
+        requestAnimationFrame(setup);
     })();
 }
